66d84c9d2de6c1fefe72159ab246ec9157a54a1c
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2022-04-14 16:13:01 +0530

    updated readme and the comments

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ This will simply show a popup message to the users when they first visit your we
 ```
 $ npm i gohomegota
 ```
+
+## 3. Usage
+
+**This works on all the JavaScript frontend frameworks.**
+
+Just import it to any global file or component like this.
+
+```
+import gohomegota from 'gohomegota';
+```
+
+And call this method.
+
+```
+gohomegota.showResistance();
+```
+
+That's it! 
+
+Feel free to customize the message contents and styles as preferred. Be creative! Make it your own!
--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 /* eslint-env node */
 function showMessage(){
+    /*
+    dialog is a new HTML5 element. You can create popups with this. Supported in most browsers currently.
+     */
     var tlDialog = document.createElement("dialog");
-    tlDialog.style.cssText = "border-radius: 10px; border: 2px solid; box-shadow: 7px 7px 15px 0px rgba(0,0,0,0.39)"
+    tlDialog.style.cssText = "border-radius: 10px; border: 2px solid; min-width:300px; box-shadow: 7px 7px 15px 0px rgba(0,0,0,0.39)"
     document.body.appendChild(tlDialog)
 
+    /*
+    Feel free to customize these elements/styles as you wish.
+     */
     let tl_title = document.createElement('p');
-    tl_title.innerText = "Small Act of Resistance!"
-    tl_title.style.fontFamily = 'Arial'
+    tl_title.innerText = "Small Act of Resistance!";
+    tl_title.style.fontFamily = 'Arial';
     tl_title.style.fontWeight = 900;
 
     let tl_hashtag = document.createElement('p');
-    tl_hashtag.innerText = "#GoHomeGota2022"
-    tl_hashtag.style.fontFamily = 'Arial'
+    tl_hashtag.innerText = "#GoHomeGota2022";
+    tl_hashtag.style.fontFamily = 'Arial';
     tl_hashtag.style.fontWeight = 100;
 
+    let tl_br = document.createElement('br');
 
-    let tl_btn = document.createElement("button");
+    let tl_btn = document.createElement("a");
     tl_btn.innerHTML = "Close";
+    tl_btn.style.cssText = "width:50px; height: 30px; border:1px solid; padding:5px; border-radius:5px; font-family:Arial; cursor:pointer"
     tl_btn.onclick = function (){
-        localStorage.setItem("tl_popup_already_seen", 'true')
+        //This will put a value in browser's local storage. So that you can save it if the user has seen the message.
+        localStorage.setItem("tl_popup_already_seen", 'true');
         tlDialog.close();
     }
 
     tlDialog.appendChild(tl_title);
     tlDialog.appendChild(tl_hashtag);
+    tlDialog.appendChild(tl_br);
     tlDialog.appendChild(tl_btn);
 
     let tl_popup_already_seen = localStorage.getItem("tl_popup_already_seen");
+    //This will check if the local storage value is present and if not, it shows the message to the user.
     if(tl_popup_already_seen !== 'true'){
         tlDialog.showModal();
     }
